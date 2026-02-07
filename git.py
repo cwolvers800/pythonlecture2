04f7a378c40f9c81274f7a84385cb2b9f2e15667
Callum Wolvers <callumwolvers@icloud.com>
2026-02-07 08:52:03 +0000

    trying to learn git

---
+++ git.py
@@ -0,0 +1,8 @@
+#Create a new repository on GitHub (don’t initialise it with anything).
+# Open your terminal or Git Bash.
+# Navigate to the folder you want to upload.
+# Initialise Git inside that folder.
+# Stage all the files in the folder.
+# Commit the files with a message.
+# Link the local folder to the GitHub repository.
+# Push the commit to GitHub.
